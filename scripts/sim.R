d6470287f0bce697e39d7085a04defbf42b208d1
Zilong-Li <zimusen94@gmail.com>
2025-09-12 15:47:41 +0200

    add two cases in simulator

--- scripts/sim.R
+++ scripts/sim.R
@@ -1,30 +1,71 @@
 deletion <- function(d, col = "darkgreen") {
-  rect(3, 2+d, 8, 4+d, col=col)
+  rect(2, 2+d, 8, 4+d, col=col)
   rect(15, 2+d, 20, 4+d, col=col)
   lines(c(8, 15), rep((2+d+4+d)/2, 2), col="black", lwd=4)
 }
 
+reference <- function(d, col = "darkgreen") {
+  rect(2, 1+d, 20, 3+d, col=col)
+}
+
 case1 <- function() {
-  # Create a plot
-  plot(1, axes = F, type="n", xlim=c(0, 20), ylim=c(0, 40), xlab="", ylab="")
-  # Vertically rotated text using srt=90
+  par(mar = rep(0, 4))
+  plot(1, axes = F, type="n", xlim=c(0, 20), ylim=c(0, 60), xlab="", ylab="")
   text(x = 1, y = 4, cex = 2, labels = 'Hap1', srt = 90)
   deletion(d = 0)
   deletion(d = 3)
-  text(x = 1, y = 14, cex = 2, labels = 'Hap2', srt = 90)
+  text(x = 1, y = 13.5, cex = 2, labels = 'Hap2', srt = 90)
   deletion(d = 9, col = "darkblue")
   deletion(d = 12, col = "darkblue")
-  rect(0., 0., 20.5, 18.5, col = NA, border = "black", lwd = 2)
+  rect(0., 0., 20.5, 17.5, col = NA, border = "black", lwd = 2)
 
-  text(x = 1, y = 32, cex = 2, labels = 'Unknown', srt = 90)
+  text(x = 1, y = 32, cex = 2, labels = 'Hap1', srt = 90)
   deletion(d = 24)
   deletion(d = 27)
   deletion(d = 30)
   deletion(d = 33)
-  rect(0., 24.5, 20.5, 40.5, col = NA, border = "black", lwd = 2)
+  rect(0., 24.5, 20.5, 40, col = NA, border = "black", lwd = 2)
+
+  text(x = 1, y = 52.5, cex = 2, labels = 'Unknown', srt = 90)
+  deletion(d = 45, col = "grey")
+  deletion(d = 48, col = "grey")
+  deletion(d = 51, col = "grey")
+  deletion(d = 54, col = "grey")
+  rect(0., 45.5, 20.5, 60, col = NA, border = "black", lwd = 2)
+
 }
 
-err <- 0.05 # error of being SV signal
+
+case2 <- function() {
+  par(mar = rep(0, 4))
+  plot(1, axes = F, type="n", xlim=c(0, 20), ylim=c(0, 70), xlab="", ylab="")
+  text(x = 1, y = 7, cex = 2, labels = 'Hap1', srt = 90)
+  reference(d = 0)
+  reference(d = 3)
+  reference(d = 6)
+  reference(d = 9)
+  text(x = 1, y = 18, cex = 2, labels = 'Hap2', srt = 90)
+  deletion(d = 15, col = "darkblue")
+  rect(0., 0., 20.5, 23.5, col = NA, border = "black", lwd = 2)
+  text(x = 1, y = 34, cex = 2, labels = 'Hap1', srt = 90)
+  reference(d = 29)
+  reference(d = 32)
+  deletion(d = 34)
+  text(x = 1, y = 45, cex = 2, labels = 'Hap2', srt = 90)
+  reference(d = 42, col = "darkblue")
+  reference(d = 45, col = "darkblue")
+  rect(0., 28.5, 20.5, 50.5, col = NA, border = "black", lwd = 2)
+  text(x = 1, y = 62, cex = 2, labels = 'Unknown', srt = 90)
+  reference(d = 55, col = "grey")
+  reference(d = 57.5, col = "grey")
+  deletion(d = 59, col = "grey")
+  reference(d = 62.5, col = "grey")
+  reference(d = 65, col = "grey")
+  rect(0., 54, 20.5, 70, col = NA, border = "black", lwd = 2)
+}
+
+
+err <- 0.01 # error of being SV signal
 
 readlike <- function(read, truth, err, hap1prob=0.99){
   if (truth[1]==-1){
@@ -59,28 +100,28 @@ glunphased <- function(nr, na, err) {
   list("GT" = names(gp)[which.max(gp)],"GP" = gp, "GQ" = as.integer(gq))
 }
 
-phasedgenotypes <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1), ncol = 2, byrow = T)
+orderedgenotypes <- matrix(c(0, 0, 1, 0, 0, 1, 1, 1), ncol = 2, byrow = T)
 
 ## hr: a vector indicates the phasing of the reads with ref
 ## ha: a vector indicates the phasing of the reads with alt
 glphased <- function(nr, hr, na, ha, err) {
   gls <- rep(0, 4)
   while(nr > 0) {
-    p <- ifelse(hr[nr] == 1, 0.99, 0) ## assume perfect phasing
+    p <- ifelse(hr[nr] == 1, 0.99, 0.01) ## assume perfect phasing
     nr <- nr - 1
-    gls[1] <- gls[1] + log10(readlike(0, phasedgenotypes[1,], err, p))
-    gls[2] <- gls[2] + log10(readlike(0, phasedgenotypes[2,], err, p))
-    gls[3] <- gls[3] + log10(readlike(0, phasedgenotypes[3,], err, p))
-    gls[4] <- gls[4] + log10(readlike(0, phasedgenotypes[4,], err, p))
+    gls[1] <- gls[1] + log10(readlike(0, orderedgenotypes[1,], err, p))
+    gls[2] <- gls[2] + log10(readlike(0, orderedgenotypes[2,], err, p))
+    gls[3] <- gls[3] + log10(readlike(0, orderedgenotypes[3,], err, p))
+    gls[4] <- gls[4] + log10(readlike(0, orderedgenotypes[4,], err, p))
   }
   
   while(na > 0) {
-    p <- ifelse(ha[na] == 1, 0.99, 0) ## assume perfect phasing
+    p <- ifelse(ha[na] == 1, 0.99, 0.01) ## assume perfect phasing
     na <- na - 1
-    gls[1] <- gls[1] + log10(readlike(1, phasedgenotypes[1,], err, p))
-    gls[2] <- gls[2] + log10(readlike(1, phasedgenotypes[2,], err, p))
-    gls[3] <- gls[3] + log10(readlike(1, phasedgenotypes[3,], err, p))
-    gls[4] <- gls[4] + log10(readlike(1, phasedgenotypes[4,], err, p))
+    gls[1] <- gls[1] + log10(readlike(1, orderedgenotypes[1,], err, p))
+    gls[2] <- gls[2] + log10(readlike(1, orderedgenotypes[2,], err, p))
+    gls[3] <- gls[3] + log10(readlike(1, orderedgenotypes[3,], err, p))
+    gls[4] <- gls[4] + log10(readlike(1, orderedgenotypes[4,], err, p))
   }
   gl00 <- gls[1]
   gl01 <- c(log10(0.5)+gls[2], log10(0.5)+gls[3])
@@ -94,21 +135,35 @@ glphased <- function(nr, hr, na, ha, err) {
   list("GT" = names(gp)[which.max(gp)],"GP" = gp, "GQ" = as.integer(gq))
 }
 
-## case1
-plotcase1 <- function() {
+plotgl <- function(r) {
   par(mar = c(4, 5, 3, 2))
-  layout(matrix(c(1, 2, 1, 3), ncol = 2, byrow = T))
-  case1()
-  r <- glunphased(0, 4, err)
-  barplot(r$GP, ylim = c(0, 1), main = paste0("GT=",r$GT,",GQ=", r$GQ), ylab = "Genotype Likelihood", cex.axis = 2, cex.names = 2, cex.lab = 2, cex.main = 2)
-  r <- glphased(0, c(0), 4, c(1, 1, 2, 2), err)
-  barplot(r$GP, ylim = c(0, 1), main = paste0("GT=",r$GT,", GQ=", r$GQ), ylab = "Genotype Likelihood", cex.axis = 2, cex.names = 2, cex.lab = 2, cex.main = 2)
+  barplot(r$GP, ylim = c(0, 1), main = paste0("GT=",r$GT,",GQ=", r$GQ), ylab = "Genotype Likelihood", cex.axis = 2, cex.names = 2, cex.lab = 2, cex.main = 2, yaxt = "n")
+  axis(2, labels=FALSE, tick=T)
 }
 
+plotcase1 <- function() {
+  layout(matrix(c(1, 1, 1, 2, 3, 4), ncol = 2))
+  case1()
+  r <- glunphased(0, 4, err)
+  plotgl(r)
+  r <- glphased(0, c(0), 4, c(1, 1, 1, 1), err)
+  plotgl(r)
+  r <- glphased(0, c(0), 4, c(1, 1, 2, 2), err)
+  plotgl(r)
+}
+
+## plotcase1()
+
+## case1
+(r <- glunphased(0, 4, err))
+
+(r <- glphased(0, c(0), 4, c(1, 1, 1, 1), err))
+
+(r <- glphased(0, c(0), 4, c(1, 1, 2, 2), err))
+
+
 ## case 2
-(r <- glunphased(4, 1, err))
 
-(r <- glphased(4, rep(1,4), 1, c(2), err))
-
-barplot(r$GP, ylim = c(0, 1), main = paste0("GT=",r$GT,",GQ=", r$GQ), ylab = "Genotype Likelihood", cex.axis = 2, cex.names = 2, cex.lab = 2)
-
+(r <- glunphased(0, 4, err))
+(r <- glphased(0, c(0), 4, c(1, 1, 1, 1), err))
+(r <- glphased(0, c(0), 4, c(1, 1, 2, 2), err))
